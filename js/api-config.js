9884b0c96e09f6d87ce88e1c3f5e50071ff5d528
jammee6-ops <jammee6@gmail.com>
2026-03-14 10:59:26 +0700

    Update api-config.js

--- js/api-config.js
+++ js/api-config.js
@@ -2,4 +2,4 @@
 //  ตั้งค่า Google Apps Script Web App URL
 //  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
 // ============================================================
-const GAS_API_URL = '';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbzX1VR0u8WFkxS6MB8pEjf63LEtpAP9WhQW-YMYSSVSDImWm7HPOM_x2Ocxp0zvgZYr/exec';
